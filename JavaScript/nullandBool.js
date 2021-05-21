10c61251a33ec1269053d155c6903686badf6b17
kath <55346310+Kathund@users.noreply.github.com>
2021-05-21 20:15:32 +0800

    Create nullandBool.js

---
+++ JavaScript/nullandBool.js
@@ -0,0 +1,13 @@
+//temp is  coming from website API
+
+var temp = 
+
+console.log("Current temperature is: " + temp)
+
+let actualMarks = 10
+
+let myGrade = (actualMarks <= 10)
+
+console.log(myGrade)
+
+//https://www.youtube.com/watch?v=vF4b1KFfA3A&list=PLRAV69dS1uWTSu9cVg8jjXW8jndOYYJPP&index=7
