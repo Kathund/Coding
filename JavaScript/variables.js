ce6da2b7ee02e695f13c9d8007f5808735180d50
kath <55346310+Kathund@users.noreply.github.com>
2021-05-21 20:14:33 +0800

    Create variables.js

---
+++ JavaScript/variables.js
@@ -0,0 +1,17 @@
+let Name = 'joe'
+
+let score = 100
+let bonus = 20
+
+let totalScore = score + bonus
+
+console.log(totalScore)
+
+let firstName = 'Joe'
+let lastName = 'Doe'
+
+let fullName = firstName + ' ' + lastName
+
+console.log(firstName + ' ' +lastName)
+
+console.log(fullName)
