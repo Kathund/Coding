f30f54507a7c245aa946e7033e2ac4dc995404e9
kath <55346310+Kathund@users.noreply.github.com>
2021-05-25 09:30:12 +0800

    Update learnIfElse.js

--- JavaScript/learnIfElse.js
+++ JavaScript/learnIfElse.js
@@ -1,5 +1,38 @@
 
-if (false) {
-    console.log('i am inside if block')
-    console.log('I am still inside')
+// if (false) {
+//     console.log('i am inside if block')
+//     console.log('I am still inside')
+// } else if (false) {
+//     console.log('i am inside IF IN CAPS BLOCK')
+
+// } else {
+//     console.log('NOTICE THIS PART')
+
+// }
+
+
+// LCO
+
+var whoIsHere = 'user'
+
+if (whoIsHere === 'user') {
+    console.log('Greeting message for user')
+    console.log('Allow access to view all courses')
+} else if (whoIsHere === 'teacher') {
+    console.log('Greeting message for TEACHER')
+    console.log('Allow access to his courses')
+} else {
+    console.log('MESSAGE: please verify your email')
+    console.log('Send user an email for verification')
 }
+
+
+
+// Grade problem
+
+// students marks :
+
+// 10 - Amaming
+// 5 - Good
+// 3 - poor
+// 0 - fail
